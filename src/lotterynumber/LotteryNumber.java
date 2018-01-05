dbb0d618f5a70f2efd63cdc16da5b44cd1abd3fc
Steven Zhang <xiaoneverregret@gmail.com>
2018-01-05 00:31:55 -0500

    improve the code

--- src/lotterynumber/LotteryNumber.java
+++ src/lotterynumber/LotteryNumber.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lotterynumber;
+
 
 import java.util.HashSet;
 import java.util.Random;
@@ -30,13 +30,24 @@ public class LotteryNumber {
         System.out.println("Enter the number of values you want to generate: ");
         int number = keyboard.nextInt();
         //generate 10 unique values and store them in a set;
-        Set <Integer> a = new HashSet();
+
+       // int min = Integer.valueOf(args[0]);
+       // int max = Integer.valueOf(args[1]);
+       // int number = Integer.valueOf(args[2]);
+       
+      if(number > (max - min+1))
+      {
+        System.out.println("can not generate "+number+" unique number within the ranger.");
+      }
+      else{
+        Set <Integer> a = new HashSet<Integer>();
         while(a.size() < number)
         {
         a.add(lottery(min, max));
         }
         System.out.println(a);
-    }
+        }
+     }
     public static int lottery(int min , int max )
     {
      int lucky;
